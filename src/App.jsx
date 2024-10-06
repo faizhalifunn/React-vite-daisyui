32e2607f978b4a1c5a56318253e11c78abf13a40
faizhalifunn <faiz.zhalifun@gmail.com>
2024-10-07 02:21:54 +0700

    belum berhasil panggil daisyui nya

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <>
     <h1>Hello there, im Faiz</h1>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!?
+    </h1>
+    <button class="btn btn-outline btn-secondary">Secondary</button>
+    <button class="btn btn-accent btn-outline">Three</button>
+    <button class="btn btn-success">Success</button>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui'),],
 }
 
